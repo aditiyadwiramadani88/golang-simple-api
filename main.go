c41fc1e90c2a7dfaee7d5bfa2b7a0e682be8a17a
aditiyadwiramadani88 <aditiyadwiramadani88@gamil.com>
2021-01-22 11:47:21 -0500

    routes

--- main.go
+++ main.go
@@ -1,25 +1,29 @@
 package main
 
 import (
-	"fmt"
+	"log"
+
+	"go-rest-crud/models"
 	"go-rest-crud/views"
 	"net/http"
 	"time"
-
 	"github.com/gorilla/mux"
 )
 
-
 func main(){ 
-
 	r :=mux.NewRouter()
-	r.HandleFunc("/", views.Index)
-
+	r.HandleFunc("/", views.Index).Methods("GET")
+	r.HandleFunc("/", views.AddData).Methods("POST")
+	r.HandleFunc("/{id:[0-9]+}", views.DetailsData).Methods("GET")
+	r.HandleFunc("/{id:[0-9]+}", views.DeleteData).Methods("DELETE")
+	r.HandleFunc("/{id:[0-9]+}", views.UpdateData).Methods("PUT")
     srv := &http.Server{
 		Handler: r,
         Addr:         ":8000",
         WriteTimeout: 15 * time.Second,
         ReadTimeout:  15 * time.Second,
-	} 
-	fmt.Println(srv.ListenAndServe())
+		} 
+	models.CreateTable()
+	log.Println(srv.ListenAndServe())
+
 }
