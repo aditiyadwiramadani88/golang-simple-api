88aaa0cf6936b275af854e28d727ccf1bd287fc8
aditiyadwiramadani88 <aditiyadwiramadani88@gamil.com>
2021-01-23 10:39:29 -0500

    Response Types

--- response/Res.go
+++ response/Res.go
@@ -3,6 +3,7 @@ import "net/http"
 
 func JsonResponse(w http.ResponseWriter, resp string, status int) (int, error) { 
 	w.Header().Add("content-type", "application/json")
+	w.Header().Add("Access-Control-Allow-Origin", "*")
 	w.WriteHeader(status)
 	return w.Write([]byte(resp))
 } 
